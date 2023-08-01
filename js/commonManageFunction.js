a88dcafa9ccdbb5a9b776e01f8614eef39e840d1
mochalt <girlriver@gmail.com>
2023-08-01 21:31:42 +0900

    관리자탭 생성

---
+++ js/commonManageFunction.js
@@ -0,0 +1,14 @@
+function makeAdminTabMenu() {
+    $("#admin_tab_menu").append(
+        "<a href='examManage.html'>exam</a>" +
+            " / " +
+            "question" +
+            " / " +
+            "user" +
+            " / " +
+            "answer-record" +
+            " / " +
+            "score"
+    );
+    $("#admin_tab_menu").append("<br/><br/>");
+}
